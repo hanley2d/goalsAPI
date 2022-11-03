c03d5de7a4990d7bae4fa8b7a9521db2546882ef
hanley2d <davidwilliamhanley@gmail.com>
2022-11-02 20:55:50 -0600

    goals controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+// error handler - overwrites default error handler
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}.`));
